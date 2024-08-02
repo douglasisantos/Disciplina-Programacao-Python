a93890bf1c95e02e2d2a08d90b93a9c3113b406e
Douglas Iracet <douglasiracet@gmail.com>
2024-08-01 21:44:03 -0300

    first commit

---
+++ Aula 1/codigo1.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome ?')
+idade = int(input('qual a sua idade ?')) 
+sono = idade / 3 
+print(nome,'Você Dormiu :', sono , 'Anos')
---
+++ Aula 1/codigo2.py
@@ -0,0 +1,8 @@
+
+import os
+os.system('cls')
+frase = input('Digite Uma Frase: ').upper()
+#frase = frase.upper()
+
+print(frase)
+print('Tamanho da Frase', len(frase))

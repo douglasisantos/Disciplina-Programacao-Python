1b9b0d782767e30bd9a3a705eabddf4183890835
DOUGLAS IRACET DO SANTOS <douglasiracet@gmail.com>
2024-08-08 21:13:42 -0300

    Update exercicioImc.py

--- Aula 2/exercicioImc.py
+++ Aula 2/exercicioImc.py
@@ -1,7 +1,7 @@
 #Faça um programa em Python que receba um nome, uma altura (em cm) e um peso (em kg). Em seguida, calcule e exiba (junto com o nome) o IMC da pessoa. O cálculo do IMC é: peso / (altura * altura), onde a altura deve estar em metros.
 
 nome = input("Digite seu Nome: ")
-altura = float(input("Digite a sua altura (cm): "))
-peso = int(input("Digite o seu peso (kg): "))
+altura = float(input("Digite a sua altura (m): "))
+peso = float(input("Digite o seu peso (kg): "))
 imc = peso / (altura * altura)
-print(nome,f"O seu IMC é: {imc:.2f}")
+print(nome,"o seu IMC é ",imc)

69fd2066182ad5ae4833fc8f3f1e91cf2f6162c4
DOUGLAS IRACET DO SANTOS <douglasiracet@gmail.com>
2024-08-09 23:41:41 -0300

    Create exercicio5.py

---
+++ Aula 2/exercicio5.py
@@ -0,0 +1,5 @@
+#Faça um Programa que converta metros para centímetros.
+metros = float(input('Informe o valor do metro: '))
+centimeros = metros * 100
+
+print('A média das notas foi :',centimeros)

25efde4d84b17e564a1646d301920987b7a57ffc
DOUGLAS IRACET DO SANTOS <douglasiracet@gmail.com>
2024-08-09 23:08:39 -0300

    Create exercicio4.py

---
+++ Aula 2/exercicio4.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+nota1 = float(input('Informe a primeira nota: '))
+nota2 = float(input('Informe segunda nota: '))
+nota3 =float(input('Informe terceira nota: '))
+nota4 = float(input('Informe quarta nota: '))
+
+media = (nota1+nota2+nota3+nota4) / 4
+print('A média das notas foi :',media)

cb2f0ba62c1d99cb63fbcd7fccbe1ce194c1955f
DOUGLAS IRACET DO SANTOS <douglasiracet@gmail.com>
2024-08-09 23:04:00 -0300

    Update exercicio3.py

--- Aula 2/exercicio3.py
+++ Aula 2/exercicio3.py
@@ -1,3 +1,4 @@
+#Faça um Programa que peça dois números e imprima a soma.
 numero1 = int(input('digite o primeiro número: '))
 numero2 = int(input('digite o segundo número: '))
 soma = numero1 + numero2
